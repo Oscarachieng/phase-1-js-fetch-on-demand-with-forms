eccec6cd42004e9cbced4f254e86cc9579314d1b
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-06-14 19:36:37 +0300

    commit tree d1576f3806d7903fb755f8de592ff83357f50afe

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const init = () => {
+    const myForm = document.querySelector("form");
+    myForm.addEventListener("submit", (event)=> {
+        event.preventDefault();
+        const input = document.querySelector("#searchByID");
+        console.log(input.value);
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response =>{
+            return response.json();
+        }).then(data =>{
+            const titleHead = document.getElementsByTagName("section#movieDetails h4")
+            titleHead.textContent = data.title
+            const summery = document.getElementsByTagName("section#movieDetails p")
+            summery.textContent = data.summery
+        });
+
+    })
   
 }
+
+//function insertAfter(newNode, existingNode) {
+ //   existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+
 
 document.addEventListener('DOMContentLoaded', init);
